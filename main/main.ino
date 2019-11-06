1aa26d40479b5f0bd491d8482c5e20a582ebed96
Kaku <kaku@telocate.de>
2019-11-06 15:29:20 +0100

    the new vitameter is not vitameter A as ble name

--- main/main.ino
+++ main/main.ino
@@ -96,7 +96,7 @@ void setup() {
   values.init();
   pedo.calibrate();
   values.pedoEnable = 1;
-  ble.init("Vitameter low energy");
+  ble.init("Vitameter low energy A");
 }
 
 
@@ -144,14 +144,13 @@ void sendDataOverUart(void) {
   ble.write("Print data over Serial Port...\n");
   Serial.println(values.prepareAllData().c_str());
 
-  /*// TODO for testing reasons
-  Serial.print("co2 flash ");
-  Serial.println(values.getCurrentCO2FlashIdx());
-  Serial.print("voc flash ");
-  Serial.println(values.getCurrentVOCFlashIdx());
-  Serial.print("uv flash ");
-  Serial.println(values.getCurrentUVIFlashIdx());
-  */
+  // TODO for testing reasons
+  Serial.print("co2 data points: ");
+  Serial.println((values.getCurrentCO2FlashIdx() + values.co2_idx)/2);
+  Serial.print("voc data points: ");
+  Serial.println(values.getCurrentVOCFlashIdx() + values.voc_idx);
+  Serial.print("uvi data points: ");
+  Serial.println(values.getCurrentUVIFlashIdx() + values.uvi_idx);
   
   delay(5000); 
   ledBlue.off();  
